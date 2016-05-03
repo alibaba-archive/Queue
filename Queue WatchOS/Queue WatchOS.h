1a032670f7a8c82ed4681836efc1fe7d79976c13
HarriesChen <info@mrchenhao.com>
2016-05-04 00:31:57 +0800

    support watch os

---
+++ Queue WatchOS/Queue WatchOS.h
@@ -0,0 +1,19 @@
+//
+//  Queue WatchOS.h
+//  Queue WatchOS
+//
+//  Created by ChenHao on 16/5/4.
+//  Copyright © 2016年 HarriesChen. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Queue WatchOS.
+FOUNDATION_EXPORT double Queue_WatchOSVersionNumber;
+
+//! Project version string for Queue WatchOS.
+FOUNDATION_EXPORT const unsigned char Queue_WatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Queue_WatchOS/PublicHeader.h>
+
+
